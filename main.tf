fbd4d5c04aff0a8754a8cb8c7b0901222f3245b6
srikanth5572 <155812214+srikanth5572@users.noreply.github.com>
2024-02-20 15:27:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "aws_instance" "web" {
 }
 resource "aws_default_subnet" "default_az1" {
 availability_zone = "ap-south-1a"
-vpc_id     = aws_vpc.default.id
   tags = {
     Name = "Default subnet for ap-south-1a"
   }
