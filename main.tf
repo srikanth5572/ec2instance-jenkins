7e091eb0c978b3be5ce4d32a5a0b6c7644d5f20b
srikanth5572 <155812214+srikanth5572@users.noreply.github.com>
2024-02-20 13:40:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_instance" "web" {
-  ami           = ami-0e670eb768a5fc3d4
+  ami           = "ami-0e670eb768a5fc3d4"
   instance_type = "t2.micro"
 
   tags = {
