416f0d354a5b7b46e259abf9b9aaa764d6ff2e34
srikanth5572 <155812214+srikanth5572@users.noreply.github.com>
2024-02-20 15:17:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,59 +9,13 @@ resource "aws_instance" "web" {
     Name = "Server"
   }
 }
-############create a vpc############
-resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
- 
- tags = {
-   Name = "Project VPC"
- }
-}
-#############create a subnet2###########
-resource "aws_subnet" "sub1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+
+resource "aws_default_subnet" "default_az1" {
+availability_zone = "ap-south-1a"
+vpc_id = "vpc-02c0c20de89ddae26"
+cidr_block = "172.31.1.0/20"
 
   tags = {
-    Name = "subnet1"
+    Name = "Default subnet for ap-south-1a"
   }
-}
-resource "aws_subnet" "sub2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/23"
-
-  tags = {
-    Name = "subnet2"
-  }
-}
-#################create a IG###########
-resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.main.id
- 
- tags = {
-   Name = "Project VPC IG"
- }
-}
-#############create  route table#########
-resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.main.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.gw.id
- }
- 
- tags = {
-   Name = "2nd Route Table"
- }
-}
-########attaching subnet1 to route table########
-resource "aws_route_table_association" "sub1" {
-  subnet_id      = aws_subnet.sub1.id
-  route_table_id = aws_route_table.second_rt.id
-}
-
-resource "aws_route_table_association" "sub2" {
-  subnet_id      = aws_subnet.sub2.id
-  route_table_id = aws_route_table.second_rt.id
 }
