9bdd888c00eb57d93ca9f0b2721d40fed3c3635b
srikanth5572 <155812214+srikanth5572@users.noreply.github.com>
2024-02-20 14:54:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,15 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0e670eb768a5fc3d4"
   instance_type = "t2.micro"
-
   tags = {
     Name = "Server"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = "vpc-02c0c20de89ddae26"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
   }
 }
